193cf3ed4c5e61b78b16b5818ac25553a95d4414
thethtetaung17 <thethtetaung17@gmail.com>
2019-12-08 12:20:29 +0630

    Index.js

---
+++ index.js
@@ -0,0 +1,4 @@
+//this is my publish npm package
+import MyTestComponent from './src/components/MyTestComponent'
+
+export{MyTestComponent}
---
+++ src/components/MyTestComponent.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import { Text, View } from 'react-native'
+
+class MyTestComponent extends Component {
+    render() {
+        return (
+            <View>
+                <Text> textInComponent </Text>
+            </View>
+        )
+    }
+}
+export default MyTestComponent
